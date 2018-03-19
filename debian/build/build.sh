1e0238b6d0e5445ae76aec131c79f88bdee37cae
Sebastien Badia <seb@sebian.fr>
2018-03-19 17:54:48 +0100

    d/build: Also build source-package

--- debian/build/build.sh
+++ debian/build/build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 mk-build-deps -i -r -t 'apt-get -y' /opt/consul-deb/debian/control
-pushd /opt/consul-deb && gbp buildpackage $GIT_BUILDPACKAGE_OPTIONS
+pushd /opt/consul-deb && gbp buildpackage $GIT_BUILDPACKAGE_OPTIONS && gbp buildpackage -S -d --git-builder="debuild -eGOPATH -eGIT_DESCRIBE -eGIT_COMMIT -i -I -uc -us"
 cp /tmp/build-area/* /tmp/consul-deb
